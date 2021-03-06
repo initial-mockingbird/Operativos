af67374cb8517857ffe2e4565a014aefeffcb5c2
MRizzo1 <46231629+MRizzo1@users.noreply.github.com>
2021-03-06 16:02:05 -0400

    INTRO_MSG modificado a intro más larga. Añadida función de mensajes conversacionales random

--- Tarea1/main.c
+++ Tarea1/main.c
@@ -6,7 +6,10 @@
 #include <stdio.h>
 
 #define MAX_LEN 100
-#define INTRO_MSG "Soy Eliza, y este es el mensaje de inicio\n\n"
+#define INTRO_MSG "Bienvenido. Acabas de ingresar a ELIZAG6, una version del bot conversacional ELIZA desarrollado por "\
+                  "IBM entre 1964 y 1966 buscando parodiar al psicologo Carl Rogers.\n\nELIZA actua como un piscoterapeuta "\
+                  "y te ayudara a que expreses tus emociones. Para lograrlo, tienes que comunicarte con «ella» como si estuvieses hablando con una persona real.\n\n"\
+                  "Debes tener en cuenta que ELIZA no es un psicoterapeuta real, por lo que solo sirve como desahogo temporal y no puede sustituir la ayuda profesional.\n\n"
 #define FALSE 0
 #define TRUE !FALSE
 
@@ -16,7 +19,7 @@
  * 
  * @param char*
  * @param Queue*
- * @return char* 
+ * @return int
  */
 int readInput(char* inp){
     char messages[12][MAX_LEN] = {"Me alegra que hayas hablado conmigo. Espero volver a verte pronto.", "Eso es muy triste de leer. Mi mas sentido pesame.",
@@ -38,6 +41,26 @@ int readInput(char* inp){
     printf("Eliza: %s\n",messages[position]);
     return 1;
     //return messages[position];
+}
+
+/**
+ * @brief Imprime un mensaje conversacional random.
+ * 
+ * @return int
+ */
+int randomMessage(){
+    char conversationalMessages[4][MAX_LEN] = {"¿Que esta pasando ahora por tu mente?", "De lo que has hecho ultimamente, ¿hay algo que quieras contarme?", 
+                                                "¿Has tenido otros psicoterapeutas? Cuentame sobre ellos.", "Dime algo de lo que te guste hacer en tu tiempo libre."};
+
+    srand (time(NULL)); 
+    int position = rand() % 4;
+
+    if (position == -1 || position > 3){
+        return 0;
+    }
+
+    printf("Eliza: %s\n",conversationalMessages[position]);
+    return 1;
 }
 
 int main(int argc, char const *argv[])
