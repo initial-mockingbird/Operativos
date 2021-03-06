b736eca2d2549d59b1f4cfda00003210f8f93585
Ka Shing <kashing1008@gmail.com>
2021-03-05 22:41:52 -0400

    anadido cableado manual de PC

--- Tarea1/Categorias.c
+++ Tarea1/Categorias.c
@@ -362,3 +362,86 @@ void crearCategorias(){
     
     categoriesToList(categories_list);
 }
+
+#define PC  "\nPRIORIDAD 1:DESPEDIDA\n\
+            Acabo el tiempo\nAdios\nChao\nDebo irme\nHasta luego\nHora de irme\nHora de irse\n\
+            Me voy\nTengo que irme\n\
+            PRIORIDAD 2: PESAME\n\
+            Cobro la vida\nCosto la vida\nEstiro la pata\nFallece\nFallecio\nMuere\nMurio\n\
+            Paso a mejor vida\nPerdio la vida\n\
+            PRIORIDAD 3: FREUDIANO\n\
+            Abuela\nAbuelo\nEs mi\nHermana\nHermano\nMadre\nMama\nPadre\nPapa\nPrima\nPrimo\n\
+            PRIORIDAD 4: EXPLORACION\n\
+            Cree\nCreo\nDice\nDigo\nHace\nHago\nIgual\nIguales\nLo mismo\nParece\nParecen\n\
+            Parecidos\nPiensa\nPienso\nRecuerda\nRecuerdo\n\
+            PRIORIDAD 5: ESPACIO SEGURO\n\
+            Deberia\nDebo\nDesgraciado\nDisculpa\nLamento\nLo Siento\nMaldito\nPerdon\nPuede\n\
+            PRIORIDAD 6: EXPLORACION SENTIMIENTOS\n\
+            Abusa\nAbuso\nAvento\nCayo\nDolor\nDuele\nEra\nEstoy\nExagera\nFue\nGane\nGano\n\
+            Hice\nHizo\nKarma\nLanzo\nLlego\nLlegue\nLogre\nLogro\nOcurre\nOcurrio\nPaso\nPerdi\nPerdio\n\
+            Soy\nTrauma\n\
+            PRIORIDAD 7: PROFUNDIZACION SENTIMIENTOS\n\
+            Agrada\nAgrado\nAguanta\nAguantar\nAguanto\nAlegre\nAlegria\nAsco\nBuena\nBueno\nColera\n\
+            Contento\nCoraje\nDesagrado\nDisgusto\nDivertida\nDivertido\nEstoy sintiendo\nFelicidad\n\
+            Feliz\nGusta\nIra\nMe siento\nMolesta\nMolestia\nMolesto\nNauseas\nPena\nSiento que\nSola\n\
+            Solo\nTriste\nTristeza\n\
+            PRIORIDAD 8: POSITIVISMO\n\
+            Aguanta\nAguante\nAguanto\nChingada\nContrario\nHorrible\nMala\nMalo\nNo\n\
+            Pesima\nPesimo\nResiste\nResistira\nResisto\nTerrible\n\
+            PRIORIDAD 9: DEFLEXION\n\
+            Como\nCual\nCuales\nCuando\nDonde\nel es\nElla es\nEres\nEstas\n\
+            Por que\nQue es\nQuien\nSois\nSos\nTu\nUsted\nVos\n\
+            PRIORIDAD 10: AYUDA\n\
+            Como funciona\nNo se\nQue debo hacer\nQue hacer\nQue hago\n\
+            PRIORIDAD 11: EXPLORACION\n\
+            Desde\nHacia\nHasta\nMediante\nMientras\nPara\nPor\nSegun\n\
+            PRIORIDAD 12: DELIMITACION\n\
+            Abrumador\nEllos\nLleva\nMuchas\nMuchos\nNos\nNosotros\nSobrecogedor\nTan\n\
+            Todas\nTodos\n"
+
+
+// /**
+//  * @brief Lee el archivo con categorías y palabras, las parsea en un diccionario que tiene la categoría con un apuntador a su lista de palabras correspondientes
+//  * 
+//  * @param categories 
+//  */
+// void readData(struct category **categories){
+//     char file_name[25], linebuf[MAX_LEN], cat_name[35], *token;
+//     struct words_list* words;
+//     int i = 0; 
+    
+//     token = strtok(&PC,"\n");
+//     while( token != NULL ) {
+//         strcpy(linebuf, token);
+//         if(linebuf[0] == '\r' || linebuf[0] == '\n'){
+//             if (i > 1) //Es decir, la lista de cateogrías ya se creó
+//                 i = 1;     
+//             continue; //Se salta el resto del while
+//         } else if (strstr(linebuf, "PRIORIDAD") != NULL || strstr(linebuf, "IDENTIFICAR") != NULL) {
+//             strcpy(cat_name, linebuf);
+//             continue; //Se salta el resto del while
+//         }
+
+//         if (i == 0){ //Si no se ha creado la lista de categorías
+//             words = generateWordList(linebuf);
+//             (*categories) = generateCategoriesList(cat_name, words);
+//             i += 2;
+//         }else if (i == 1){ //La lista de categorías está creada, pero se necesita insertar una categoría nueva.
+//             words = generateWordList(linebuf);
+//             appendCategory(cat_name, words, *categories);
+//             i++;
+//         }else{ //Añadimos palabras a la categoría en la que nos encontramos.
+//             appendWord(linebuf, words);
+//         }
+
+//         token = strtok(NULL, " ");
+//     }
+
+//     //PARA HACER PRUEBAS: printf("Primera Categoria: %s y Novena Palabra: %s\n", categories->name, categories->words->next->next->next->next->next->next->next->next->value);
+//     //printf("Primera Categoria: %s y Novena Palabra: %s\n", categories->name, categories->words->next->next->next->next->next->next->next->next->value);
+// }
+
+
+
+
+
