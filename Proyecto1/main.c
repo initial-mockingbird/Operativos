aa584c78424d796a399f94a2d500d75bae3a1fbb
Ka Shing <kashing1008@gmail.com>
2021-04-09 16:46:35 -0400

    Nuevos archivos

---
+++ Proyecto1/main.c
@@ -0,0 +1,13 @@
+#include "../Utils/LinkedList/LinkedList.h"
+#include "Calculo.h"
+#include "Etapa1.h"
+#include "Etapa2.h"
+#include "Lectura.h"
+#include "Mensaje.h"
+#include "Modelos.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(int args, char *argv[]){
+    return EXIT_SUCCESS;
+}
