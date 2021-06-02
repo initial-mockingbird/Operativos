efb3eb069f1ec1a386171f7a605b255e89eb3a8a
MRizzo1 <46231629+MRizzo1@users.noreply.github.com>
2021-06-02 13:10:51 -0400

    Parseo de flags y mensaje para help

--- Proyecto2/Readdir.c
+++ Proyecto2/Readdir.c
@@ -8,6 +8,14 @@
 #include <unistd.h>
 #include <stdio.h>
 
+#define helpMessage  "Modo de empleo:  <dir> [-method|-f <file>]\n"\
+                        "Evalua los resultados de una elección estadounidense.\n"\
+                        "dir es el directorio conteniendo los votos (ver más adelante)\n"\
+                        "[-method] es uno de los siguientes flags\n"\
+                        "\t-FPTP, -WTA indicando que en todos los Estados deben asignarse los votos electorales al candidato con mayoría simple en el Estado\n"\
+                        "\t-split, -dist indicando que el voto correspondiente a cada Distrito Electoral debe asignarse al candidato que obtenga mayoría simple\n"\
+                        "\t--NPIVC, -pop indicando que los votos del Colegio Electoral deben asignarse al candidato que obtenga mayoría simple a nivel nacional\n"\
+                        "-h, --help muestra esta ayuda y finaliza\n"
 #define MAX_LEN 100
 #define MAX_LEVEL 3
 char* concatenateStrings(char dst[], char* str1, char* str2, char* str3){
@@ -64,7 +72,29 @@ int readAllDir(char* fileDir, char* dirName, int level){
 }
 
 
-int main(){
+int main(int argc, char *argv[]){
+    
+    if(argc < 2 || (argc > 2 && argc < 5)) {
+        printf("Falta un argumento. \nPrueba --help o -h para más información.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(argc == 2 && (COMPSTR(argv[1], "-h") == 1 || COMPSTR(argv[1], "--help") == 1)) {
+        printf(helpMessage);
+        exit(EXIT_SUCCESS);
+    } 
+
+    char dir[25], method[10], file[15];
+    
+    strcpy(dir, argv[1]);
+    strcpy(file, argv[4]);
+
+    if(COMPSTR(argv[2], "-FPTP") != 1 && COMPSTR(argv[2], "-WTA") != 1 && COMPSTR(argv[2], "-split") != 1 && COMPSTR(argv[2], "-dist") != 1 && COMPSTR(argv[2], "--NPIVC") != 1 && COMPSTR(argv[2], "-pop") != 1){
+        printf("Argumento invalido. \nPrueba --help o -h para más información.\n");
+    }
+    strcpy(method, argv[2]);
+    
+    
     readAllDir("./0", "", 0);
     return EXIT_SUCCESS;
 }
