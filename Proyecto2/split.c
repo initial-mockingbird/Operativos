eddfa87ba8bc435c6f40d6fae31f51e7246f0fea
initial-mockingbird <daniel.andres.pinto@gmail.com>
2021-06-02 21:21:09 -0400

    ambos metodos listos T.T

--- Proyecto2/split.c
+++ Proyecto2/split.c
@@ -33,6 +33,19 @@ typedef struct splitData{
 
 }splitData;
 
+// Posible dummy structure, encapsula todas las cosas que necesito para hacer los calculos
+typedef struct npvicData{
+    Reporte* pais;                  // Para el modo National Popular Vote Interstate Compact, basta con el reporte pais
+    char*  candidato1;              // Nombre del candidato 1.
+    char*  candidato2;              // Nombre del candidato 2.
+    char*  estado;                  // nombre del estado.
+    //sem_t* mutexVotosColegio;     // el mutex para poder modificar cuantos votos de COLEGIO ELECTORAL tienen los candidatos (globalmente)
+    int*   colegioGlob1;            // apuntador a la cantidad de votos del colegio electoral (global) que tiene el candidato 1.
+    int*   colegioGlob2;            // apuntador a la cantidad de votos del colegio electoral (global) que tiene el candidato 2.
+    int*   estadosFinalizados;      // apuntador a un entero que senaliza cuantos estados faltan por finalizar
+
+}npvicData;
+
 
 typedef struct RowData{
     char* candidato;     // Nombre del candidato
@@ -45,7 +58,7 @@ typedef struct RowData{
 void printHeader();
 void printRow(RowData*, RowData*);
 void split(splitData*);
-
+void npivc(npvicData*);
 void printHeader(){
     printf("Nombre Candidato | Nombre Estado | Metodo Usado | Votos Colegio Electoral | Porcentaje de Votos Bruto\n");
 }
@@ -53,6 +66,58 @@ void printHeader(){
 void printRow(RowData *candidato1, RowData *candidato2){
     printf("%-16s | %-13s | %-12s | %23d | %25.3f \n",candidato1->candidato, candidato1->estado, candidato1->metodo, candidato1->colegioLoc, candidato1->porcentajeLoc);
     printf("%-16s | %-13s | %-12s | %23d | %25.3f \n",candidato2->candidato, candidato2->estado, candidato2->metodo, candidato2->colegioLoc, candidato2->porcentajeLoc);
+}
+
+
+void npivc(npvicData* data){
+    // saca el % del ganador y perdedor de una por q q  fastidio.
+    float porcentajeGanador  = MAX(data->pais->cand1, data->pais->cand2) / (data->pais->cand1 + data->pais->cand2) * 100;
+    float porcentajePerdedor = 100 - porcentajeGanador;
+    int colegioLoc1     = 0;
+    int colegioLoc2     = 0;
+    float porcentajeLoc1;
+    float porcentajeLoc2;
+    
+    // decide quien gana.
+    if (data->pais->cand1 > data->pais->cand2){
+        colegioLoc1 += 3;
+        porcentajeLoc1 = porcentajeGanador;
+        porcentajeLoc2 = porcentajePerdedor;
+        
+    } else if (data->pais->cand2 > data->pais->cand1)
+    {
+        colegioLoc2 += 3;
+        porcentajeLoc2 = porcentajeGanador;
+        porcentajeLoc1 = porcentajePerdedor;
+    } else {
+        perror("EMPATE A NIVEL NACIONAL, INSOLITO!");
+    }
+    
+
+    RowData* candidato1Str = (RowData*) malloc(sizeof(RowData));
+    candidato1Str->candidato     = data->candidato1;
+    candidato1Str->colegioLoc    = colegioLoc1;
+    candidato1Str->estado        = data->estado;
+    candidato1Str->metodo        = "NIPVC";
+    candidato1Str->porcentajeLoc = porcentajeLoc1;
+    
+    RowData* candidato2Str = (RowData*) malloc(sizeof(RowData));
+    candidato2Str->candidato     = data->candidato2;
+    candidato2Str->colegioLoc    = colegioLoc2;
+    candidato2Str->estado        = data->estado;
+    candidato2Str->metodo        = "NIPVC";
+    candidato2Str->porcentajeLoc = porcentajeLoc2;
+
+    printRow(candidato1Str, candidato2Str);
+
+    // Y pedir el mutex para actualizar las cosas compartidas
+    /*
+    sem_wait(data->mutexVotosColegio);
+    *(data->colegioGlob1) += colegioLoc1;
+    *(data->colegioGlob2) += colegioLoc2;
+    (*(data->estadosFinalizados))--;
+    sem_post(data->mutexVotosColegio);
+    */
 }
 
 void split(splitData* data){
@@ -130,7 +195,7 @@ void split(splitData* data){
     *(data->colegioGlob1) += colegioLoc1;
     *(data->colegioGlob2) += colegioLoc2;
     (*(data->estadosFinalizados))--;
-    sem_post(mutex);
+    sem_post(data->mutexVotosColegio);
     */
 
 
